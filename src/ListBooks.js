e5f3bd0e3c80546d25fbe5d96fbee48f18d84fe5
Lee Savarese <lee.s@nc.rr.com>
2018-09-20 09:08:35 -0400

    Static version built

---
+++ src/ListBooks.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import BookShelf from './BookShelf';
+
+class ListBooks extends Component {
+	render() {
+		return (
+			<div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <BookShelf bookShelfName="Currently Reading"/>
+                <BookShelf bookShelfName="Want to Read"/>
+                <BookShelf bookShelfName="Read"/>
+              </div>
+            </div>      
+            <div className="open-search">
+              <Link to="/search">Add a book</Link>
+            </div>
+          </div>
+		)
+	}
+}
+
+export default ListBooks
